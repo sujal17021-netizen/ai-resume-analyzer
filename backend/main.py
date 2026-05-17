6c3612029130864e0c319fe94c1efb5d60682fd4
Sujal Gupta <sujal17021@gmail.com>
2026-05-18 01:43:01 +0530

    fixed gemini model path

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 # Home route
 @app.get("/")
